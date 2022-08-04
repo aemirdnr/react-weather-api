7aa5007dc035bb4ea1b896059c1e495d5deb1dab
aemirdnr <aemirdnr@gmail.com>
2022-08-04 23:38:10 +0300

    Weather API & Redesign

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -2,48 +2,27 @@ import "./Weather.sass";
 import { useWeather } from "../../context/WeatherContext";
 
 function Weather() {
-  const { city } = useWeather();
+  const { city, weather, err } = useWeather();
 
   return (
     <div className="container d-flex flex-column align-items-center mt-5">
       <h2>{city}</h2>
-      <div className="row justify-content-center gap-3 mt-4">
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Monday</span>
-          <span>30 °C</span>
+      {!err ? (
+        <div className="d-flex justify-content-center gap-3 mt-4">
+          <div className="weather__card">
+            <span>{weather.temp + " °C"}</span>
+            <img
+              src={
+                "http://openweathermap.org/img/wn/" + weather.icon + "@2x.png"
+              }
+              alt={weather.alt}
+            />
+            <span>{new Date().toLocaleString()}</span>
+          </div>
         </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Tuesday</span>
-          <span>30 °C</span>
-        </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Wednesday</span>
-          <span>30 °C</span>
-        </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Thursday</span>
-          <span>30 °C</span>
-        </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Friday</span>
-          <span>30 °C</span>
-        </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Saturday</span>
-          <span>30 °C</span>
-        </div>
-        <div className="weather__card col-1">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" />
-          <span>Sunday</span>
-          <span>30 °C</span>
-        </div>
-      </div>
+      ) : (
+        <h1 className="mt-5">Couldn't find any data for this city.</h1>
+      )}
     </div>
   );
 }
--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,17 +1,46 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const WeatherContext = createContext();
 
 export const WeatherProvider = ({ children }) => {
   const [city, setCity] = useState("İstanbul");
+  const [weather, setWeather] = useState({});
+  const [err, setErr] = useState(false);
 
   useEffect(() => {
-    console.log(city);
+    const API =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=" +
+      process.env.API_KEY;
+
+    axios(API)
+      .then((res) => {
+        setWeather({
+          city: res.data.name,
+          temp: res.data.main.temp,
+          icon: res.data.weather[0].icon,
+          type: res.data.weather[0].main,
+          alt: res.data.weather[0].description,
+        });
+
+        setErr(false);
+
+        console.log(weather);
+      })
+      .catch((error) => {
+        setErr(true);
+        console.log(error);
+        console.log(err);
+      });
   }, [city]);
 
   const values = {
     city,
     setCity,
+    weather,
+    err,
   };
 
   return (
